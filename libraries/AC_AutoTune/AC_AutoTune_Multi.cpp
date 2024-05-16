3bdcc26c616cba09a2f056db3fc6bf31c9f9ac6f
Leonard Hall <leonardthall@gmail.com>
2024-05-17 09:27:47 +0930

    Copter: Autotune: Remove double call to AC_Attitude_Control

--- libraries/AC_AutoTune/AC_AutoTune_Multi.cpp
+++ libraries/AC_AutoTune/AC_AutoTune_Multi.cpp
@@ -1211,7 +1211,6 @@ void AC_AutoTune_Multi::twitch_test_run(AxisType test_axis, const float dir_sign
     // hold current attitude
 
     if ((tune_type == SP_DOWN) || (tune_type == SP_UP)) {
-        attitude_control->input_rate_bf_roll_pitch_yaw(0.0f, 0.0f, 0.0f);
         // step angle targets on first iteration
         if (twitch_first_iter) {
             twitch_first_iter = false;
@@ -1230,7 +1229,9 @@ void AC_AutoTune_Multi::twitch_test_run(AxisType test_axis, const float dir_sign
                 // request yaw to 20deg
                 attitude_control->input_angle_step_bf_roll_pitch_yaw(0.0f, 0.0f, dir_sign * target_angle);
                 break;
-            }
+            } 
+        } else {
+            attitude_control->input_rate_bf_roll_pitch_yaw(0.0f, 0.0f, 0.0f);
         }
     } else {
         attitude_control->input_rate_bf_roll_pitch_yaw_2(0.0f, 0.0f, 0.0f);
